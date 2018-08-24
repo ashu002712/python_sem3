a36105e6d840446c795202355ad69f446245f9b0
Ashutosh Singh <39981591+ashu002712@users.noreply.github.com>
2018-08-24 14:19:37 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,10 @@
+name={"firstname":"aditiya","lastname":"Singh"}
+salary=200000
+contact=["9856756232","8956784562"]
+emailid=["aadi@gmail.com","aaditiyasingh@gmail.com"]
+employee=[name,salary,contact,emailid]
+
+for i in range(len(employee)):
+    print(employee[i])
+#employee={"empid":"101","empName":"Aditiya","salary":200000,"contact":"9005647852","Designation":"sales maneger","emailid":"aditiya@gmail.com"}
+
